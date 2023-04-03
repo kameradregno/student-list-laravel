a88c446efd779aac82c39c4661ccbaa8be5f3b64
kameradregno <regon0705@gmail.com>
2023-04-03 10:38:14 +0800

    responsive layout

--- resources/views/student/create.blade.php
+++ resources/views/student/create.blade.php
@@ -6,34 +6,36 @@
 
     {{-- <h3 class="mb-3">Tambah Siswa</h3> --}}
 
-    <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">Nama Siswa</th>
-                <th scope="col">Kelas</th>
-                <th></th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <form method="POST" action="{{ url('student') }}" class="mt-5">
-                    @csrf
-                    <td>
-                        <div class="mb-3">
-                            <input type="text" class="form-control rounded-0" style="width: auto;" name="nama"
-                                id="nama" aria-describedby="helpId" placeholder="" required>
-                        </div>
-                    </td>
-                    <td>
-                        <div class="mb-3">
-                            <input type="text" class="form-control rounded-0" style="width: auto;" name="kelas"
-                                id="kelas" aria-describedby="helpId" placeholder="" required>
-                        </div>
-                    </td>
-                    <td> <button type="submit" class="btn btn-outline-dark rounded-0 mx-3">Simpan Data</button></td>
-                </form>
-            </tr>
-        </tbody>
-    </table>
+    <div class="col-sm-auto col-md-auto mx-auto mx-lg-auto mx-md-auto mx-sm-auto">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Nama Siswa</th>
+                    <th scope="col">Kelas</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <form method="POST" action="{{ url('student') }}" class="mt-5">
+                        @csrf
+                        <td>
+                            <div class="mb-3">
+                                <input type="text" class="form-control rounded-0" style="width: auto;" name="nama"
+                                    id="nama" aria-describedby="helpId" placeholder="" required>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="mb-3">
+                                <input type="text" class="form-control rounded-0" style="width: auto;" name="kelas"
+                                    id="kelas" aria-describedby="helpId" placeholder="" required>
+                            </div>
+                        </td>
+                        <td> <button type="submit" class="btn btn-outline-dark rounded-0 mx-3">Simpan Data</button></td>
+                    </form>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 
 @endsection
