a46cc1232938381af65dcc094e6126906413b4f9
kameradregno <regon0705@gmail.com>
2023-04-03 10:39:58 +0800

    tes

--- resources/views/student/edit.blade.php
+++ resources/views/student/edit.blade.php
@@ -4,43 +4,43 @@
 
 @section('content')
 
-    <div class="col-sm-auto col-md-auto mx-auto mx-lg-auto mx-md-auto mx-sm-auto">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">No</th>
-                    <th scope="col">Nama Siswa</th>
-                    <th scope="col">Kelas</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                @php $counter = 1 @endphp
-                <tr>
-                    <th scope="row">{{ $counter++ }}</th>
-                    <form method="post" action="{{ url("student/$student->id") }}">
-                        @method('patch')
-                        @csrf
-                        <td>
-                            <div class="mb-3">
-                                <input type="text" value="{{ $student->nama }}" class="form-control rounded-0"
-                                    style="width: auto;" name="nama" id="siswa" aria-describedby="helpId"
-                                    placeholder="">
-                            </div>
-                        </td>
-                        <td>
-                            <div class="mb-3">
-                                <input type="text" value="{{ $student->kelas }}" class="form-control rounded-0"
-                                    style="width: auto;" name="kelas" id="kelas" aria-describedby="helpId"
-                                    placeholder="">
-                            </div>
-                        </td>
-                        <td> <button type="submit" class="btn btn-outline-dark mx-3 rounded-0">Edit Data</button></td>
-                    </form>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">No</th>
+                <th scope="col">Nama Siswa</th>
+                <th scope="col">Kelas</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            @php $counter = 1 @endphp
+            <tr>
+                <th scope="row">{{ $counter++ }}</th>
+                <form method="post" action="{{ url("student/$student->id") }}">
+                    @method('patch')
+                    @csrf
+                    <td>
+                        <div class="mb-3">
+                            <input type="text" value="{{ $student->nama }}" class="form-control rounded-0" style="width: auto;"
+                                name="nama" id="siswa" aria-describedby="helpId" placeholder="">
+                        </div>
+                    </td>
+                    <td>
+                        <div class="mb-3">
+                            <input type="text" value="{{ $student->kelas }}" class="form-control rounded-0" style="width: auto;"
+                                name="kelas" id="kelas" aria-describedby="helpId" placeholder="">
+                        </div>
+                    </td>
+                    <td> <button type="submit" class="btn btn-outline-dark mx-3 rounded-0">Edit Data</button></td>
+                </form>
+            </tr>
+        </tbody>
+    </table>
 
 
 @endsection
+
+
+
