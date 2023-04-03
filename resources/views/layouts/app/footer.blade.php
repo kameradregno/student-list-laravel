35d6603becb7710a9ac3d1f7e007b778484eb6ca
kameradregno <regon0705@gmail.com>
2023-04-03 10:16:07 +0800

    add footer in layout

---
+++ resources/views/layouts/app/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <p class="col-md-4 mb-0 text-body-secondary"><b>DataSekul</b></p>
+
+
+
+    <ul class="nav col-md-4 justify-content-end">
+      <li class="nav-item"><a href="{{ url('student') }}" class="nav-link px-2 text-body-secondary">Home</a></li>
+    </ul>
+  </footer>
